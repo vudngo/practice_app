fe7d00fb03d3969bd527cbaf14428b7324515125
Vu Ngo <vu@invoca.com>
2015-01-15 15:06:47 -0800

    all scaffold

---
+++ app/models/company.rb
@@ -0,0 +1,6 @@
+class Company < ActiveRecord::Base
+  attr_accessible :contact_name, :contact_number, :email, :name
+
+
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PracticeApp::Application.config.secret_token = '1d43e704c7cffbe91c930b902cdecbd89e3dcf1801e4502dae3f73f98e2e937be67871f4ae18e7cfab96459be1693bb3e7a7913df9f8e07b0e6ec9ee24a4f900'
